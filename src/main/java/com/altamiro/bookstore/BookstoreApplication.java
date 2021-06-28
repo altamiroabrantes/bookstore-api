cb985b17ef9a1e30ab44d56d8849c87ab5009244
altamiroabrantes <altamiro.abrantes@gmail.com>
2021-06-28 16:14:18 -0300

    Perfil de teste implementado e troca da versão do spring 2.4.7 para 2.3.7.RELEASE

--- src/main/java/com/altamiro/bookstore/BookstoreApplication.java
+++ src/main/java/com/altamiro/bookstore/BookstoreApplication.java
@@ -1,42 +1,12 @@
 package com.altamiro.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.altamiro.bookstore.domain.Categoria;
-import com.altamiro.bookstore.domain.Livro;
-import com.altamiro.bookstore.repositories.CategoriaRepository;
-import com.altamiro.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
+public class BookstoreApplication {
 
-	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private LivroRepository livroRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro livro1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsu,", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(livro1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(livro1));
-	}
-
 }
---
+++ src/main/java/com/altamiro/bookstore/service/DBService.java
@@ -0,0 +1,41 @@
+package com.altamiro.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.altamiro.bookstore.domain.Categoria;
+import com.altamiro.bookstore.domain.Livro;
+import com.altamiro.bookstore.repositories.CategoriaRepository;
+import com.altamiro.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public void instanciaBaseDeDados() {
+		
+		
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+		
+		Livro livro1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsu,", cat1);
+		Livro livro2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsu,", cat1);
+		Livro livro3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsu,", cat1);
+		Livro livro4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsu,", cat1);
+		Livro livro5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsu,", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(livro1, livro2));
+		cat2.getLivros().addAll(Arrays.asList(livro3, livro4, livro5));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(livro1, livro2, livro3, livro4, livro5));
+	}
+}
