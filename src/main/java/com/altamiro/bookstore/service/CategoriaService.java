920b7992cb98f96a069495a5dc95ca65370dc696
altamiroabrantes <altamiro.abrantes@gmail.com>
2021-07-05 14:58:46 -0300

    Exceção personalizada para delete de Categoria.

--- src/main/java/com/altamiro/bookstore/service/CategoriaService.java
+++ src/main/java/com/altamiro/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.altamiro.bookstore.domain.Categoria;
@@ -22,11 +23,11 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return categoriaRepository.save(obj);
@@ -41,6 +42,11 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		categoriaRepository.deleteById(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.altamiro.bookstore.service.Exceptions.DataIntegrityViolationException(
+					"Categoria não pode ser deletada! Possui livros associados.");
+		}
 	}
 }
